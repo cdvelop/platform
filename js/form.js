2bc53c6d872562a26b10b6a7ae11759c311dc161
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-08-24 23:01:06 -0400

    update module: model refactor js InputNormal

--- js/form.js
+++ js/form.js
@@ -1,19 +1,26 @@
-function InputRight(input) {
-    console.log("INPUT Right RECIBIDO: ", input);
-
+function updateInputClass(input, classesToAdd, classesToRemove) {
     let fd = input.closest('fieldset');
     if (fd != null) {
-        fd.classList.add("foka");
-        fd.classList.remove("ferr");
+        if (classesToAdd && classesToAdd.length > 0) {
+            fd.classList.add(...classesToAdd);
+        }
+        if (classesToRemove && classesToRemove.length > 0) {
+            fd.classList.remove(...classesToRemove);
+        }
     }
-};
+}
+
+function InputRight(input) {
+    // console.log("INPUT Right RECIBIDO: ", input);
+    updateInputClass(input, ["foka"], ["ferr"]);
+}
 
 function InputWrong(input) {
-    console.log("INPUT Wrong RECIBIDO: ", input);
+    // console.log("INPUT Wrong RECIBIDO: ", input);
+    updateInputClass(input, ["ferr"], ["foka"]);
+}
 
-    let fd = input.closest('fieldset');
-    if (fd != null) {
-        fd.classList.remove("foka");
-        fd.classList.add("ferr");
-    }
-};
+function InputNormal(input) {
+    // console.log("INPUT normal RECIBIDO: ", input);
+    updateInputClass(input, [], ["ferr", "foka"]);
+}
