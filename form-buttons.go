07713ba273e56238d562bd9cac8926fb086622cc
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-11-08 12:06:06 -0300

    update module: model add print and reset html buttons in form

--- form-buttons.go
+++ form-buttons.go
@@ -1,33 +1,27 @@
 package platform
 
-func buttons() string {
+func buttons(module_name, object_name string) string {
 
 	out := `<div class="crud-buttons-container">
 	<div class="contebuton">`
 
-	out += `  <button type="button" name="btn_delete" title="Eliminar" disabled="true">
-	<svg aria-hidden="true" focusable="false" class="form-btn">
-		<use xlink:href="#icon-btn-delete" />
-	</svg>
-</button>`
-
-	out += `<button type="button" name="btn_cancel" title="Cancelar" disabled="true">
+	out += `<button type="button" data-module="` + module_name + `" name="btn_cancel" title="Comenzar Nuevamente" onclick="resetModule(this)">
 	<svg aria-hidden="true" focusable="false" class="form-btn">
 		<use xlink:href="#icon-btn-cancel" />
 	</svg>
-</button>`
-
-	out += `<button type="button" name="btn_add" title="Iniciar Captura de Imagen">
-	<svg aria-hidden="true" focusable="false" class="form-btn">
-	<use xlink:href="#icon-btn-new" />
-	</svg>
 	</button>`
 
-	out += `<!-- <button type="button" name="btn_save" title="Tomar Fotografia" disabled>
+	out += `  <button type="button" data-object_name="` + object_name + `" name="btn_printer" title="Imprimir" onclick="printForm(this)">
 	<svg aria-hidden="true" focusable="false" class="form-btn">
-		<use xlink:href="#icon-camera" />
+		<use xlink:href="#icon-printer" />
 	</svg>
-</button> -->`
+</button>`
+
+	// 	out += `<button type="button" name="btn_save" title="Tomar Fotografía" disabled>
+	// 	<svg aria-hidden="true" focusable="false" class="form-btn">
+	// 		<use xlink:href="#icon-camera" />
+	// 	</svg>
+	// </button>`
 
 	out += `</div>
 </div>`
